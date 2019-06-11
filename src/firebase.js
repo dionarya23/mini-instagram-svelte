c677693ae91872f11186eef5f40fcc1e640d8309
Dion Arya Pamungkas <dionarya.p@gmail.com>
2019-06-11 11:04:22 +0700

    change readme

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import 'firebase/firestore'
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDlaqwNzzqYahvxN4qf1f3Ut84-hJZXJw8",
-    authDomain: "small-talk-42dc0.firebaseapp.com",
-    databaseURL: "https://small-talk-42dc0.firebaseio.com",
-    projectId: "small-talk-42dc0",
-    storageBucket: "small-talk-42dc0.appspot.com",
-    messagingSenderId: "1091250868353",
-    appId: "1:1091250868353:web:0d9f1a6e7283ba52"
+   //yout credential
 }
 
 firebase.initializeApp(firebaseConfig)
